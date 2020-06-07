1832b597ec51e8fe84bd669c3b8cd84aa04e5862
Brandon Barnett <6238678+brbarnett@users.noreply.github.com>
2020-06-07 16:30:07 -0500

    Camera and script tweaks

---
+++ Assets/FollowCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public Vector3 OffsetFromPlayer = new Vector3(0f, 0f, 0f);
+
+    public PlayerController Player;
+
+    protected void LateUpdate()
+    {
+        if (Player == null) return;
+
+        transform.position = Player.transform.position + OffsetFromPlayer;
+        transform.LookAt(Player.transform);
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+
+    void Start()
+    {
+        this._rigidbody = this.GetComponent<Rigidbody>() ?? throw new Exception("Cannot find rigidbody");
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            print("W key was pressed");
+        }
+    }
+}
